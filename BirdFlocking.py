c09ad70a97aabf1c3b3e01faece44c2aa6169789
Unknown <simonspammen@gmail.com>
2019-01-29 11:27:56 +0100

    Init Commit, outline of class structure

---
+++ BirdFlocking.py
@@ -0,0 +1,41 @@
+###############################################
+# ITECH 2020 Computational Design Thinking Final Assignment
+# By Guillaume, Simon, Tim, Gabriel
+###############################################
+# 3D Printing Agent-Based BirdFlocking
+###############################################
+
+
+
+import Rhino.Geometry as rg
+
+class AgentSystem:
+    #Creating different types of agents
+    #Realtime flocking optimization using R-Tree system.
+
+    pass
+
+class Agent:
+    def __init__():
+        pass
+
+    def ComputeFoodToNestVector(self):
+        pass
+
+    def ComputeUpliftVector(self):
+        pass
+
+    def ComputeFlockingVector(self):
+        pass
+
+    def ComputeWindVector(self):
+        pass
+
+    def ComputePredatorAvoidVector(self):
+        pass
+
+    def ComputeAvoidObstacleVector(self):
+        pass
+
+class Predator(Agent):
+    pass
